ab39b8fa26fca3ab7e2dd06b4401284dd252ac7e
Peter Vlk <petervlk86@gmail.com>
2022-12-13 10:59:00 +0100

    add day10 solution - part 2

--- src/vlko/day10.clj
+++ src/vlko/day10.clj
@@ -1,12 +1,7 @@
 (ns vlko.day10
   (:require
-   [clojure.java.io :as io]
-   [clojure.string :as s]))
-
-(def state
-  {:sum []
-   :register 1
-   :completed-cycles 0})
+   [clojure.string :as s]
+   [vlko.io]))
 
 (defn instruction-cycles
   [instruction]
@@ -17,25 +12,54 @@
       (conj base-cycles {:completed-cycles 1
                          :register (Integer/valueOf x)}))))
 
-(defn reduce-fn
+(defn signal-strength-collector
   [state instruction-cycle]
   (let [new-state (merge-with + state instruction-cycle)]
     (if-not (zero? (mod (- (:completed-cycles new-state) 20) 40))
       new-state
       (update new-state :sum conj (* (:completed-cycles new-state) (:register state))))))
 
+(defn execute-instructions
+  [init-state reduce-fn instructions]
+  (->> instructions
+      (mapcat instruction-cycles)
+      (reduce reduce-fn init-state)))
 
 (defn signal-strength-sum
   [instructions]
   (->> instructions
-       (mapcat instruction-cycles)
-       (reduce reduce-fn state)
+       (execute-instructions
+         {:sum [] :register 1 :completed-cycles 0}
+         signal-strength-collector)
        :sum
        (apply +)))
 
 (defn solution
   []
-  (->> (io/resource "input10.txt")
-       slurp
-       s/split-lines
-       signal-strength-sum))
+  (signal-strength-sum (vlko.io/file-lines "input10.txt")))
+
+(defn render-fn
+  [state instruction-cycle]
+  (let [current-row-idx (mod (:completed-cycles state) 40)
+        display (if (zero? current-row-idx) (conj (:display state) []) (:display state))
+        current-row (peek display)
+        rendered-display-rows (pop display)
+        sprite (:register state)
+        pixel (if (<= (dec sprite) current-row-idx (inc sprite)) \# \.)]
+    (merge-with
+      +
+      (assoc state :display (conj rendered-display-rows (conj current-row pixel)))
+      instruction-cycle)))
+
+(defn crt-renderer
+  [instructions]
+  (->> instructions
+       (execute-instructions
+         {:display [] :register 1 :completed-cycles 0}
+         render-fn)
+       :display
+       (map #(apply str %))))
+
+(defn solution-bonus
+  []
+  (crt-renderer (vlko.io/file-lines "input10.txt")))
